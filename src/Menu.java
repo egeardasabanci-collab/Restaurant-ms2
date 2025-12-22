b4f3b22fb422b46d234c1abfa097599a45abd69b
serdar282 <tekinyusufserdar@gmail.com>
2025-12-22 11:15:18 +0300

    Refactor Menu methods for better clarity
    humanized menu class but still wrond output.

--- src/Menu.java
+++ src/Menu.java
@@ -1,24 +1,26 @@
-
 import java.util.List;
 
 public class Menu {
 
-    public static void show(List<Product> products) {
-        System.out.println("\n=== MENU ===");
-        for (Product p : products) {
-            System.out.println(
-                    p.getId() + " - " +
-                            p.getName() + " | " +
-                            p.getPrice() + "₺ | Stock: " +
-                            p.getPiece()
-            );
+    public static void showthemenu(List<Product> productlist) {
+        System.out.println("MENU");
+
+        for (int i = 0; i < productlist.size(); i++) {
+            Product product = productlist.get();
+            System.out.print( product.getId());
+            System.out.print(product.getName() + "PRİCE:" + product.getPrice() + "TL");
+            System.out.println("Stock: " + product.getPiece());
         }
-        System.out.println("0 - Finish");
+
+        System.out.println("log out / end the shopping");
     }
 
-    public static Product select(List<Product> products, int id) {
-        for (Product p : products)
-            if (p.getId() == id) return p;
+    public static Product ProductById(List<Product> list, int choosenId) {
+        for (Product p : list) {
+            if (p.getId() == choosenId) {
+                return p;
+            }
+        }
         return null;
     }
 }
